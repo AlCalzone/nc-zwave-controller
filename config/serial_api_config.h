2a1ad12ad43b3b27c8ce492f3f8672cbaff54950
Dominic Griesel <d.griesel@gmx.net>
2024-11-28 14:20:14 +0100

    board-specific modifications

--- config/serial_api_config.h
+++ config/serial_api_config.h
@@ -20,6 +20,21 @@
 #define SERIAL_API_PERIPHERAL_NO                 0
 #endif
 
+// USART0 TX on PA08
+#ifndef SERIAL_API_TX_PORT                      
+#define SERIAL_API_TX_PORT                       SL_GPIO_PORT_A
+#endif
+#ifndef SERIAL_API_TX_PIN                       
+#define SERIAL_API_TX_PIN                        8
+#endif
+
+// USART0 RX on PA07
+#ifndef SERIAL_API_RX_PORT                      
+#define SERIAL_API_RX_PORT                       SL_GPIO_PORT_A
+#endif
+#ifndef SERIAL_API_RX_PIN                       
+#define SERIAL_API_RX_PIN                        7
+#endif
 // [USART_SERIAL_API]$
 
 // <<< sl:end pin_tool >>>
